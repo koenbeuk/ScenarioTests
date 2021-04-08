2ebf1ccee263c89b7823d49e7919c9084b179ed4
Koen Bekkenutte <2912652+kbekkenutte@users.noreply.github.com>
2021-04-09 00:04:54 +0800

    Support for source mapping back to original scenario

---
+++ src/Linker.ScenarioTests/ScenarioFactAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Sdk;
+
+namespace ScenarioTests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [XunitTestCaseDiscoverer("ScenarioTests.Internal.ScenarioFactTestCaseDiscoverer", "Linker.ScenarioTests")]
+    public sealed class ScenarioFactAttribute : FactAttribute
+    {
+        public ScenarioFactAttribute()
+        {
+            if (!Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+        }
+
+        public string FileName { get; set; }
+        public int LineNumber { get; set; }
+    }
+}
